3dbd31a9042b35ff8789d0e1e49a72fab6dd1213
VNavrockij <VNavrockij@users.noreply.github.com>
2026-04-21 10:16:53 +0300

    Initial commit

---
+++ Kittek/KittekApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittekApp.swift
+//  Kittek
+//
+//  Created by Vitalii on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct KittekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
